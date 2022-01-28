8747d4ecd016b557a0005949042ac92060dd5ae2
Nahuel Espinosa <nahue.espinosa@gmail.com>
2022-01-28 12:25:14 -0300

    Fix Argenprop title

--- providers/argenprop.py
+++ providers/argenprop.py
@@ -25,7 +25,7 @@ class Argenprop(Provider):
                 break
 
             for prop in properties:
-                title = prop.find('h3', class_='card__title')['title']
+                title = prop.find('p', class_='card__title').get_text().strip()
                 price_section = prop.find('p', class_='card__price')
                 if price_section is not None:
                     title = title + ' ' + price_section.get_text().strip()
