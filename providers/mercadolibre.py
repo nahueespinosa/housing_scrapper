b6c4ba323fe542a7e19effe18f2011a0849d3217
Nahuel Espinosa <nahue.espinosa@gmail.com>
2022-01-30 02:08:05 -0300

    Add mercadolibre tests

--- providers/mercadolibre.py
+++ providers/mercadolibre.py
@@ -11,7 +11,6 @@ class Mercadolibre(Provider):
     async def props_from_source(self, source: str):
         page_link = self.config['base_url'] + source + '_NoIndex_True'
         from_ = 1
-        regex = r'(MLA-\d*)'
 
         while True:
             content = BeautifulSoup(await self.request(page_link), 'lxml')
@@ -21,11 +20,12 @@ class Mercadolibre(Provider):
                 break
 
             for prop in properties:
-                section = prop.find('a', class_='ui-search-result__link')
-                if section is None:
-                    section = prop.find('a', class_='ui-search-result__content')
+                section = prop.find('a', class_=['ui-search-result__link', 'ui-search-result__content'])
                 href = section['href']
-                matches = re.search(regex, href)
+                matches = re.search(r'https:.*_JM', href)
+                assert matches is not None
+                href = matches.group(0)
+                matches = re.search(r'(MLA-\d*)', href)
                 assert matches is not None
                 internal_id = matches.group(1).replace('-', '')
                 title_section = section.find('div', class_='ui-search-item__group--title')
@@ -36,8 +36,11 @@ class Mercadolibre(Provider):
                                internal_id=internal_id,
                                provider=self.name)
 
-            from_ += 48
+            from_ += len(properties)
             filter_index = source.find('_')
-            page_link = self.config['base_url'] + \
-                source[:filter_index] + f'_Desde_{from_}' + \
-                source[filter_index:] + '_NoIndex_True'
+            if filter_index != -1:
+                page_link = self.config['base_url'] + \
+                    source[:filter_index] + f'_Desde_{from_}' + \
+                    source[filter_index:] + '_NoIndex_True'
+            else:
+                page_link = self.config['base_url'] + source + f'_Desde_{from_}' + '_NoIndex_True'
